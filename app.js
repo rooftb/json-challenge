01589ad7ccaeb3225894c26294bd835c72ecf495
Tanner <tanner.roof@outlook.com>
2021-05-13 09:26:11 -0400

    Completed more problems

--- app.js
+++ app.js
@@ -21,6 +21,19 @@ console.log(
 
 // Step Two: use a map function to the full dataset to retrive the index of the objects whose tags include "mollit"
 
+data.map((i) =>
+  i.tags.includes('mollit') ? console.log(data.indexOf(i)) : null
+);
+
 //Step Three: use a forEach function on the full dataset to retrieve the names of the objects which have 2 or more friends
+data.forEach((i) => (i.friends.length >= 2 ? console.log(i.name) : null));
 
 //Step Four: use a reduce function on the full dataset to find the total balance of all active users (isActive = true).
+console.log(
+  '$' +
+    data
+      .filter((i) => i.isActive)
+      .map((i) => +i.balance.slice(1, i.length).replace(/,/g, ''))
+      .reduce((a, b) => a + b)
+      .toFixed(2)
+);
